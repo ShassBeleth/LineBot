48b8018af6f93fd8031b14f666d27e729aa3857c
ShassBeleth <shassbeleth@gmail.com>
2017-05-05 17:27:26 +0900

    ASP.Netテンプレート追加

---
+++ Parrot/Parrot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Parrot.WebApiApplication" Language="C#" %>
---
+++ Parrot/Parrot/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は、以下を通して制御されます
+// 情報を COM コンポーネントに変更するには、これらの属性の値を変更してください。
+// これらの属性の値を変更します。
+[assembly: AssemblyTitle( "Parrot" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "Parrot" )]
+[assembly: AssemblyCopyright( "Copyright ©  2017" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// ComVisible を false に設定すると、アセンブリ内のタイプが非表示になります
+// 次の場所からこのアセンブリ内の型にアクセスする必要がある場合、
+//COM、その型の ComVisible 属性を true に設定します。
+[assembly: ComVisible( false )]
+
+// このプロジェクトが COM 属性セットに公開される場合、次の GUID は typelib の ID 用です。
+[assembly: Guid( "019c0338-4914-4fb4-bc47-fa652fb55685" )]
+
+// アセンブリのバージョン情報は、次の 4 つの値で構成されます:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// すべての値を指定するか、リビジョンおよびビルド番号を既定値にできます 
+// 以下に示すように '*' を使用します:
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
