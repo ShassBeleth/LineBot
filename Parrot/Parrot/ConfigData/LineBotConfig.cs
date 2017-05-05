a407a1ccf111358facd1f6d524d9de2d3eead6f8
ShassBeleth <shassbeleth@gmail.com>
2017-05-05 21:19:29 +0900

    BotのConfigクラス作成

---
+++ Parrot/Parrot/ConfigData/LineBotConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace Parrot.ConfigData {
+
+	/// <summary>
+	/// コンフィグクラス
+	/// </summary>
+	public static class LineBotConfig {
+
+		/// <summary>
+		/// 通知に対するリプライを飛ばすURL
+		/// </summary>
+		public static readonly string ReplyMessageUrl = "";
+
+		/// <summary>
+		/// Botを一意に決定づけるトークン
+		/// </summary>
+		public static readonly string ChannelAccessToken = "";
+
+
+	}
+
+}
